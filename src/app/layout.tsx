07a64b174c01955cd0a37135c64f3d4bdbb8d3cf
101autogroup <christian@101autogroup.com>
2026-03-31 10:53:42 -0700

    Update layout.tsx

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -13,6 +13,16 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
       <body className="min-h-screen bg-white text-ink-900 antialiased">
         <Providers>{children}</Providers>
       </body>
+<!-- Google tag (gtag.js) -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=G-5Z13V7V5JW"></script>
+<script>
+  window.dataLayer = window.dataLayer || [];
+  function gtag(){dataLayer.push(arguments);}
+  gtag('js', new Date());
+
+  gtag('config', 'G-5Z13V7V5JW');
+</script>
+      
     </html>
   );
 }
